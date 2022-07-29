c720e7f6a131bda370f026187944aa6a931f42c4
PremKumbhar <kumbharprem7738@gmail.com>
2022-07-29 11:32:09 +0530

    [Prem]added UC3 file

--- EmpWage.java
+++ EmpWage.java
@@ -2,23 +2,28 @@ package com.bridgelabz.employeewageprogramme;
 
 public class EmpWage 
 {
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 		System.out.println("Welcome to Employee wage program");
 		
 		int IS_FULL_TIME=1;
 		int wagePerHour=20;
 		int fullDayHour=8;
+		int partTimeHour=4;
 		
-		double empCheck = Math.floor(Math.random() * 10) % 2;
+		double empCheck = Math.floor(Math.random() * 10) % 3;
 		if (empCheck == IS_FULL_TIME) 
 		{
 			System.out.println("Employee is present");
 			System.out.println("Employee daily wage=" +fullDayHour*wagePerHour);
 		}
-		else
+		else if (empCheck == 0)
 		{
 			System.out.println("Employee is absent");
+		}
+		else
+		{
+			System.out.println("Employee working as a part time");
+			System.out.println("Employee part time wage" +partTimeHour*wagePerHour);
 		}
 	}
 
