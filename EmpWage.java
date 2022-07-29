1e3ee8db67914fa28467b7137e33642f9e413eac
PremKumbhar <kumbharprem7738@gmail.com>
2022-07-29 11:24:15 +0530

    [Prem]added UC1 file

---
+++ EmpWage.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.employeewageprogramme;
+
+public class EmpWage 
+{
+	public static final int IS_FULL_TIME = 1;
+
+	public static void main(String[] args)
+	{
+			System.out.println("Welcome to Employee wage program");
+			double empCheck = Math.floor(Math.random() * 10) % 2;
+			if (empCheck == IS_FULL_TIME)
+				System.out.println("Employee is present");
+			else
+				System.out.println("Employee is absent");
+	}
+
+}
