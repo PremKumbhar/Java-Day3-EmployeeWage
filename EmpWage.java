fc73765af8eed54527caa2463cf487ff7ddbe739
PremKumbhar <kumbharprem7738@gmail.com>
2022-07-29 11:28:55 +0530

    [Prem]added UC2 file

--- EmpWage.java
+++ EmpWage.java
@@ -2,16 +2,25 @@ package com.bridgelabz.employeewageprogramme;
 
 public class EmpWage 
 {
-	public static final int IS_FULL_TIME = 1;
-
-	public static void main(String[] args)
+	public static void main(String[] args) 
 	{
-			System.out.println("Welcome to Employee wage program");
-			double empCheck = Math.floor(Math.random() * 10) % 2;
-			if (empCheck == IS_FULL_TIME)
-				System.out.println("Employee is present");
-			else
-				System.out.println("Employee is absent");
+		System.out.println("Welcome to Employee wage program");
+		
+		int IS_FULL_TIME=1;
+		int wagePerHour=20;
+		int fullDayHour=8;
+		
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == IS_FULL_TIME) 
+		{
+			System.out.println("Employee is present");
+			System.out.println("Employee daily wage=" +fullDayHour*wagePerHour);
+		}
+		else
+		{
+			System.out.println("Employee is absent");
+		}
 	}
+
 
 }
